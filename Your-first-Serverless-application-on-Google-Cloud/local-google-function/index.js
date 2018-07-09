729f2885dc279696b2d334a41e4b92fa6acb537c
kchowhan <kchowhan@expedia.com>
2018-07-09 10:57:51 -0500

    add local google function code

---
+++ Your-first-Serverless-application-on-Google-Cloud/local-google-function/index.js
@@ -0,0 +1,8 @@
+exports.helloWorldHttp = (req, res) => {
+    if (req.body.message === undefined) {
+      res.status(400).send('No message defined in the request body!');
+    } else {
+      console.log(req.body.message);
+      res.status(200).send('Hello World!, ' + req.body.message);
+    }
+  };
